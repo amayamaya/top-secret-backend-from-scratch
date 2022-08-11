3937dcb5242be3161ddbeca6979833be36059dd6
‘AMAYA <aamaya547@gmail.com>
2022-08-11 15:12:11 -0700

    added sign in test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserServices = require('../lib/services/UserServices');
+const UserServices = require('../lib/services/UserServices');
 
 //dummy user data
 const mockUser = {
@@ -10,34 +10,40 @@ const mockUser = {
   password: '12345',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
-//   const agent = request.agent(app);
+  const agent = request.agent(app);
 
-//   const user = await UserServices.create({ ...mockUser, ...userProps });
+  const user = await UserServices.create({ ...mockUser, ...userProps });
 
-//   const { email } = user;
-//   await agent.post('/api/v1/auth/login').send({ email, password });
-//   console.log({ user });
-//   return [agent, user];
-// };
+  const { email } = user;
+  await agent.post('/api/v1/auth/login').send({ email, password });
+  console.log({ user });
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.only('creates a new user', async () => {
+  it('creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
-    const { firstName, lastName, email } = mockUser;
+    const { email } = mockUser;
 
     expect(res.body).toEqual({
       id: expect.any(String),
-      firstName,
-      lastName,
       email,
     });
+  });
+
+  it('signs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@example.com', password: '12345' });
+    expect(res.status).toEqual(200);
   });
 
   afterAll(() => {
